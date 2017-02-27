c295ae577eae8c250e09a050656dbd77246b2386
Anton Tananaev <anton.tananaev@gmail.com>
2017-02-27 23:06:53 +1300

    Finish activity straight away

--- app/src/main/java/com/tananaev/calculator/MainActivity.java
+++ app/src/main/java/com/tananaev/calculator/MainActivity.java
@@ -8,8 +8,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         ((MainApplication) getApplication()).showNotification();
+        finish();
     }
 
 }
